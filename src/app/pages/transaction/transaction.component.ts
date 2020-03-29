c05f4b3542193d67d289e3f4950f3b5442736b04
Mark Samuel Nicasio <marksamuel.nicasio@gmail.com>
2020-03-30 02:21:41 +0800

    color of button indicated file attached

--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Inject , OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { FirebaseService, AuthService, TransactionService } from '../../shared';
-import { MatDialog, MatDialogRef , MatDialogConfig , MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MatDialogConfig, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-transaction',
@@ -14,8 +16,8 @@ export class TransactionComponent implements OnInit {
 
   transactions: Array<any>;
   activeTransactions: Array<any>;
-  managerDisapprovedTransactions : Array<any>;
-  cancelledTransactions : Array<any>;
+  managerDisapprovedTransactions: Array<any>;
+  cancelledTransactions: Array<any>;
   completedTransactions: Array<any>;
   public transactionSub: Subscription;
   uid: String;
@@ -23,27 +25,27 @@ export class TransactionComponent implements OnInit {
 
   constructor(public fbs: FirebaseService,
     public transactionService: TransactionService,
-    public dialog: MatDialog, 
+    public dialog: MatDialog,
     public authService: AuthService) {
-    }
+  }
 
   ngOnInit() {
     this.uid = sessionStorage.getItem('session-user-uid')
     this.getUserTransactions();
   }
 
-  getUserTransactions(){
-    this.transactionSub = this.transactionService.getTransaction(this.uid ).subscribe( res => {
+  getUserTransactions() {
+    this.transactionSub = this.transactionService.getTransaction(this.uid).subscribe(res => {
       this.transactions = res;
-      this.activeTransactions = this.transactions.filter( res => res.isCompleted === false && res.isApproved === false && res.isDisapproved === false && res.isCancelled === false );
-      this.managerDisapprovedTransactions = this.transactions.filter( res => res.isCompleted === false && res.isApproved === false && res.isDisapproved === true);
-      this.cancelledTransactions = this.transactions.filter( res => res.isCompleted === false  && res.isCancelled === true );
-      this.completedTransactions = this.transactions.filter( res => res.isCompleted === true && res.isApproved === true && res.isDisapproved === false && res.isCancelled === false );
+      this.activeTransactions = this.transactions.filter(res => res.isCompleted === false && res.isApproved === false && res.isDisapproved === false && res.isCancelled === false);
+      this.managerDisapprovedTransactions = this.transactions.filter(res => res.isCompleted === false && res.isApproved === false && res.isDisapproved === true);
+      this.cancelledTransactions = this.transactions.filter(res => res.isCompleted === false && res.isCancelled === true);
+      this.completedTransactions = this.transactions.filter(res => res.isCompleted === true && res.isApproved === true && res.isDisapproved === false && res.isCancelled === false);
     });
   }
 
 
-  openViewTransaction(value , status): void {
+  openViewTransaction(value, status): void {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = {
       transactionID: value.id,
@@ -65,11 +67,11 @@ export class TransactionComponent implements OnInit {
     this.dialog.open(ViewSaleTransactionComponent, dialogConfig).afterClosed().subscribe(result => {
       this.getUserTransactions();
     });
-    
+
   }
 
   ngOnDestroy() {
-    if(this.transactionSub != null){
+    if (this.transactionSub != null) {
       this.transactionSub.unsubscribe();
     }
   }
@@ -78,34 +80,34 @@ export class TransactionComponent implements OnInit {
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector : 'view-sale-transaction-dialog',
-  templateUrl : './dialog/view-transaction-dialog.html',
+  selector: 'view-sale-transaction-dialog',
+  templateUrl: './dialog/view-transaction-dialog.html',
   styleUrls: ['./transaction.component.scss'],
 })
 
 export class ViewSaleTransactionComponent {
 
-  public stage : number;
-  public buttonConfig : string;
+  public stage: number;
+  public buttonConfig: string;
 
   constructor(
     public trasactionService: TransactionService,
     public dialogRef: MatDialogRef<ViewSaleTransactionComponent>,
     public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any
-    ) {
-      this.stage = this.data.stage;
-      this.buttonConfig = this.data.buttonConfig;
-    }
+  ) {
+    this.stage = this.data.stage;
+    this.buttonConfig = this.data.buttonConfig;
+  }
 
-    cancelTransaction(){
-      this.trasactionService.cancelTransaction(this.data.transactionID)
-      this.dialogRef.close();
-    }
+  cancelTransaction() {
+    this.trasactionService.cancelTransaction(this.data.transactionID)
+    this.dialogRef.close();
+  }
 
-    uploadDocuments(){
-      const dialogConfig = new MatDialogConfig();
-      dialogConfig.data = {
+  uploadDocuments() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
       transactionID: this.data.transactionID,
       stage: this.stage,
       buttonConfig: this.buttonConfig
@@ -113,11 +115,11 @@ export class ViewSaleTransactionComponent {
     this.dialog.open(UploadDocumentComponent, dialogConfig).afterClosed().subscribe(result => {
       this.dialogRef.close();
     });
-    }
+  }
 
-    editDocuments(){
-      const dialogConfig = new MatDialogConfig();
-      dialogConfig.data = {
+  editDocuments() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
       transactionID: this.data.transactionID,
       stage: this.stage,
       buttonConfig: this.buttonConfig
@@ -125,48 +127,80 @@ export class ViewSaleTransactionComponent {
     this.dialog.open(EditDocumenComponent, dialogConfig).afterClosed().subscribe(result => {
       this.dialogRef.close();
     });
-    }
+  }
 
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector : 'edit-doc-dialog',
-  templateUrl : './dialog/upload-doc-dialog.html',
+  selector: 'edit-doc-dialog',
+  templateUrl: './dialog/upload-doc-dialog.html',
   styleUrls: ['./transaction.component.scss'],
 })
 
 export class UploadDocumentComponent {
-
+  toUpload = [{ desc: 'Buyer Information Sheet', file: undefined },
+  { desc: 'Reservation Fee', file: undefined },
+  { desc: 'Reservation Agreement', file: undefined },
+  { desc: 'Valid Goverment ID 1', file: undefined },
+  { desc: 'Valid Goverment ID 2', file: undefined },
+  { desc: 'Proof of Income', file: undefined },
+  { desc: 'Proof of Billing', file: undefined },
+  { desc: 'Payment Schedule Scheme', file: undefined },
+    // { desc: 'Others', file: undefined }
+  ]
   constructor(
     public trasactionService: TransactionService,
     public dialogRef: MatDialogRef<UploadDocumentComponent>,
     public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any
-    ) {
-      
+  ) {
+
+  }
+  selectFile(event){
+    //this.toUpload[indexOfelement]['file']= file[indexOfelement]
+    console.log(event);
+    var getidno = (event.target.id).substring(3);
+    var tonum = parseInt(getidno);
+    this.toUpload[tonum]['file'] = event.target.files[0];
+
+    if(this.toUpload[tonum]['file']){
+      document.getElementById('upbtn_'+getidno).classList.remove('btn-primary');
+      document.getElementById('upbtn_'+getidno).classList.add('btn-success');
+    }else{
+      document.getElementById('upbtn_'+getidno).classList.add('btn-primary');
+      document.getElementById('upbtn_'+getidno).classList.remove('btn-success');
     }
 
-    uploadDocuments(){
-      this.trasactionService.uploadDocuments(this.data.transactionID);
-      this.dialogRef.close();
-    }
+    console.log(this.toUpload)
 
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-  
+
+  }
+  upclick(indexOfelement){
+    $('#up_'+indexOfelement).click();
+    
+  }
+
+  uploadDocuments() {
+    this.trasactionService.uploadDocuments(this.data.transactionID);
+    this.dialogRef.close();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
 
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector : 'edit-doc-dialog',
-  templateUrl : './dialog/edit-doc-dialog.html',
+  selector: 'edit-doc-dialog',
+  templateUrl: './dialog/edit-doc-dialog.html',
   styleUrls: ['./transaction.component.scss'],
 })
 
@@ -179,17 +213,17 @@ export class EditDocumenComponent {
     public dialog: MatDialog,
     public authService: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: any
-    ) {
-      
-    }
+  ) {
 
-    editDocuments(){
-      this.dialogRef.close();
-    }
+  }
 
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
+  editDocuments() {
+    this.dialogRef.close();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
 
 
--- src/app/shared/services/files.service.ts
+++ src/app/shared/services/files.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FileModel } from '@shared/models/file.model';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
@@ -20,36 +21,79 @@ export class FilesService {
       })
     });
   }
-  getFile(id) {
-    return new Promise(resolve => {
-      this.firestore.collection<FileModel>('filesStored',
-        ref =>
-          ref.where('id', '==', id)
-      ).snapshotChanges().subscribe(data => {
-        //console.log()
-        if (data.length == 0) {
-          console.log(data.length)
-          resolve('')
-          
-          return
-        } else {
-          return data.map(e => {
-            console.log(id)
-            if (id === e.payload.doc.id) {
-              var x =
-                {
-                  id: e.payload.doc.id,
-                  ...e.payload.doc.data()
-                } as any;
-              resolve(x);
-              return x;
-            }
-          })
-        }
+  async upload_in_storage(path, file, uid, category) {
 
-      });
+    var file1 = {
+      name: file.name,
+      lastModified: file.lastModified,
+      lastModifiedDate: file.lastModifiedDate,
+      webkitRelativePath: file.webkitRelativePath,
+      size: file.size,
+      type: file.type
+    };
+    var thisclass = this;
+
+    return new Promise(function (resolve, reject) {
+      console.log(file);
+
+      var storageRef = firebase.storage().ref(path);
+
+      var task = storageRef.put(file);
+      task.then(function (snapshot) {
+        snapshot.ref.getDownloadURL().then(async function (url) {
+          const id = await thisclass.firestore.createId();
+          var fileprop = {
+            id: id,
+            fileProperties: file1,
+            uidUploaded: uid,
+            section: 'BMS',
+            fileName: `storeFile${new Date().getTime()}_${file.name}`,
+            category: category,
+            photoURL: url,
+            path: path
+          };
+          await thisclass.createFile(fileprop);
+          resolve(fileprop);
+        })
+      })
+
     })
+  }
+  async createFile(fl: any) {
+    //return this.accountCollection.add(acc);
+    var thisclass = this;
+    return new Promise(function (resolve, reject) {
+    
+      const f: FileModel = {
+        id: fl.id,
+        //id: id,
+        fileProperties: fl.fileProperties,
+        uidUploaded: fl.uidUploaded,
+        section: 'BSRE',
+        fileName: fl.fileName,
+        category: fl.category,
+        photoURL: fl.photoURL,
+        path: fl.path
+      };
+      
+      thisclass.fileCollection.doc(fl.id).set(f);
+      resolve(f);
+    })
+  }
 
+  getFile(id) {
+    var thisclass = this;
+    console.log(id)
+    return new Promise(function (resolve) {
+      thisclass.firestore.collection('filesStored').doc(id).ref.get()
+        .then(doc => {
+          var project = {
+            id: doc.id,
+            ...doc.data()
+          }
+          resolve(project)
+        });
+    })
   }
 
 }
