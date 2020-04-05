b6c32a57850b17b13d2a648e29dcc8d5d4a4a8a7
Mark Samuel Nicasio <marksamuel.nicasio@gmail.com>
2020-04-05 18:52:46 +0800

    upload image and edit client done

---
+++ src/app/shared/services/client.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private db: AngularFirestore) { }
+  updateClient(id, values, file) {
+    if (file) {
+      this.db.collection('client').doc(id).update({
+        photoURL : file
+      })
+    }
+    this.db.collection('client').doc(id).update({
+      firstName: values.firstName,
+      lastName: values.lastName,
+      fullName: values.fullName,
+      userName: values.userName,
+      contactNumber: values.contactNumber,
+      addressStreet: values.addressStreet,
+      addressTown: values.addressTown,
+      addressCity: values.addressCity,
+      addressRegion: values.addressRegion,
+    })
+  }
+}
--- src/app/shared/services/file.service.ts
+++ src/app/shared/services/file.service.ts
@@ -4,6 +4,7 @@ import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/fire
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
@@ -41,6 +42,65 @@ export class FileService {
           }
           resolve(project)
         });
+    })
+  }
+  async upload_in_storage(path, file, uid, category) {
+
+    var file1 = {
+      name: file.name,
+      lastModified: file.lastModified,
+      lastModifiedDate: file.lastModifiedDate,
+      webkitRelativePath: file.webkitRelativePath,
+      size: file.size,
+      type: file.type
+    };
+    var thisclass = this;
+
+    return new Promise(function (resolve, reject) {
+      console.log(file);
+
+      var storageRef = firebase.storage().ref(path);
+
+      var task = storageRef.put(file);
+      task.then(function (snapshot) {
+        snapshot.ref.getDownloadURL().then(async function (url) {
+          const id = await thisclass.firestore.createId();
+          var fileprop = {
+            id: id,
+            fileProperties: file1,
+            uidUploaded: uid,
+            section: 'BMS',
+            fileName: `storeFile${new Date().getTime()}_${file.name}`,
+            category: category,
+            photoURL: url,
+            path: path
+          };
+          await thisclass.createFile(fileprop);
+          resolve(fileprop);
+        })
+      })
+
+    })
+  }
+  async createFile(fl: any) {
+    //return this.accountCollection.add(acc);
+    var thisclass = this;
+    return new Promise(function (resolve, reject) {
+    
+      const f: FileModel = {
+        id: fl.id,
+        //id: id,
+        fileProperties: fl.fileProperties,
+        uidUploaded: fl.uidUploaded,
+        section: 'BSRE',
+        fileName: fl.fileName,
+        category: fl.category,
+        photoURL: fl.photoURL,
+        path: fl.path
+      };
+      
+      thisclass.fileCollection.doc(fl.id).set(f);
+      resolve(f);
     })
   }
 }
